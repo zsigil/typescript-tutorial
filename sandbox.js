f9eefcea3df4d350edcd6a2cf56c659fa7cf544a
Zsigil <chickpox@freemail.hu>
2022-12-14 10:54:53 +0100

    arrays and objects

--- sandbox.js
+++ sandbox.js
@@ -7,3 +7,28 @@ var circ = function (diameter) {
     return diameter * Math.PI;
 };
 console.log(circ(5));
+//arrays
+var names = ["luigi", "mario", "yoshi"];
+names.push("jack"); //ok
+//names.push(3); //not ok
+//if we want mixed array, declare at initialization
+var mixed = ["luigi", 3, 4, "sarah"];
+mixed.push(3);
+mixed.push("jack");
+mixed[0] = 1;
+// objects
+var ninja = {
+    name: "mario",
+    belt: "black",
+    age: 30
+};
+ninja.name = "kate";
+ninja.age = 40;
+// ninja.age = 'ryu' //not working
+// ninja.skills = ['fighting', 'sneaking'] //this property does not exist, won't work
+//the ninja object can be changed, but properties must remain the same
+ninja = {
+    name: "yoshi",
+    belt: "orange",
+    age: 30
+};
--- sandbox.ts
+++ sandbox.ts
@@ -10,3 +10,33 @@ const circ = (diameter: number): number => {
 };
 
 console.log(circ(5));
+
+//arrays
+let names = ["luigi", "mario", "yoshi"];
+names.push("jack"); //ok
+//names.push(3); //not ok
+
+//if we want mixed array, declare at initialization
+let mixed = ["luigi", 3, 4, "sarah"];
+mixed.push(3);
+mixed.push("jack");
+mixed[0] = 1;
+
+// objects
+let ninja = {
+  name: "mario",
+  belt: "black",
+  age: 30,
+};
+
+ninja.name = "kate";
+ninja.age = 40;
+// ninja.age = 'ryu' //not working
+// ninja.skills = ['fighting', 'sneaking'] //this property does not exist, won't work
+
+//the ninja object can be changed, but properties must remain the same
+ninja = {
+  name: "yoshi",
+  belt: "orange",
+  age: 30,
+};
