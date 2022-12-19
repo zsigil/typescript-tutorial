1876aee7429299faf54734139ad0fd1ffb0f92fa
Zsigil <chickpox@freemail.hu>
2022-12-19 10:25:59 +0100

    function basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,25 @@
 "use strict";
-console.log("test");
+let greet;
+greet = () => {
+    console.log();
+};
+//c is optional parameter
+// optional and default parameters go last
+const add = (a, b, c) => {
+    console.log(a + b);
+    console.log(c); //undefined, if not passed in
+};
+add(5, 10);
+//c is optional parameter, but has default value; NO '?' after c
+//inferred return type: void
+const add2 = (a, b, c = 10) => {
+    console.log(a + b);
+    console.log(c); //10, if nothing is passed in
+};
+add2(5, 10);
+//explicit return type
+// 'void' : does not return anything, just does something
+const minus = (a, b) => {
+    return a - b;
+};
+let result = minus(10, 7);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,31 @@
-console.log("test");
+let greet: Function;
+
+greet = () => {
+  console.log();
+};
+
+//c is optional parameter
+// optional and default parameters go last
+const add = (a: number, b: number, c?: number | string) => {
+  console.log(a + b);
+  console.log(c); //undefined, if not passed in
+};
+
+add(5, 10);
+
+//c is optional parameter, but has default value; NO '?' after c
+//inferred return type: void
+const add2 = (a: number, b: number, c: number | string = 10) => {
+  console.log(a + b);
+  console.log(c); //10, if nothing is passed in
+};
+
+add2(5, 10);
+
+//explicit return type
+// 'void' : does not return anything, just does something
+const minus = (a: number, b: number): number => {
+  return a - b;
+};
+
+let result = minus(10, 7);
