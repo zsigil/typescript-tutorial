69ecaf1a1dbb45f4119c42a503effe7be4446dd4
Zsigil <chickpox@freemail.hu>
2022-12-19 10:32:22 +0100

    type aliases

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,25 +1,8 @@
 "use strict";
-let greet;
-greet = () => {
-    console.log();
+//TYPE ALIASES
+const logDetails = (uid, item) => {
+    console.log(`${item} has a uid of ${uid}`);
 };
-//c is optional parameter
-// optional and default parameters go last
-const add = (a, b, c) => {
-    console.log(a + b);
-    console.log(c); //undefined, if not passed in
+const greet = (user) => {
+    console.log(`Hello, ${user.name}`);
 };
-add(5, 10);
-//c is optional parameter, but has default value; NO '?' after c
-//inferred return type: void
-const add2 = (a, b, c = 10) => {
-    console.log(a + b);
-    console.log(c); //10, if nothing is passed in
-};
-add2(5, 10);
-//explicit return type
-// 'void' : does not return anything, just does something
-const minus = (a, b) => {
-    return a - b;
-};
-let result = minus(10, 7);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,31 +1,15 @@
-let greet: Function;
+//TYPE ALIASES
 
-greet = () => {
-  console.log();
+type StringOrNum = string | number;
+type objWName = {
+  name: string;
+  uid: StringOrNum;
 };
 
-//c is optional parameter
-// optional and default parameters go last
-const add = (a: number, b: number, c?: number | string) => {
-  console.log(a + b);
-  console.log(c); //undefined, if not passed in
+const logDetails = (uid: StringOrNum, item: string) => {
+  console.log(`${item} has a uid of ${uid}`);
 };
 
-add(5, 10);
-
-//c is optional parameter, but has default value; NO '?' after c
-//inferred return type: void
-const add2 = (a: number, b: number, c: number | string = 10) => {
-  console.log(a + b);
-  console.log(c); //10, if nothing is passed in
+const greet = (user: objWName) => {
+  console.log(`Hello, ${user.name}`);
 };
-
-add2(5, 10);
-
-//explicit return type
-// 'void' : does not return anything, just does something
-const minus = (a: number, b: number): number => {
-  return a - b;
-};
-
-let result = minus(10, 7);
