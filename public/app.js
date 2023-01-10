4046763b9d38deb3c3fe06000af17a8034a05fbb
Zsigil <chickpox@freemail.hu>
2023-01-10 12:01:38 +0100

    rendering HTML element

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
 import { Invoice } from "./classes/Invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
 import { Payment } from "./classes/Payment.js";
 //using css class, typescript will not know if it is a form!=> type casting
 const form = document.querySelector(".new-item-form");
@@ -7,6 +8,9 @@ const type = document.querySelector("#type");
 const tofrom = document.querySelector("#tofrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+// list template instance
+const ul = document.querySelector("ul"); //it is sure we have an 'ul'
+const list = new ListTemplate(ul);
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     let doc;
@@ -16,5 +20,5 @@ form.addEventListener("submit", (e) => {
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
+    list.render(doc, type.value, "end");
 });
